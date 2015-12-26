848b99a66fb087b7db780c972a0d4b4bcdf720e4
Cooper Maa <coopermaa77@gmail.com>
2015-12-26 13:19:33 +0800

    Fix last typo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis', port=6379))
+redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 
 @app.route('/')
 def hello():
